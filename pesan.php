d1942c16f5d5a4e26f7f3ee9affcc53fc00814e1
AlamAbdillah <115331322+AlamAbdillah@users.noreply.github.com>
2022-12-01 09:09:50 +0700

    Update pesan.php

--- pesan.php
+++ pesan.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kedai Cesa</title>
+    <title>My Toko</title>
     <link rel="stylesheet" type="text/css" href="css/pesan.css">
 </head>
 <body>

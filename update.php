83d7965c5f84aeec88816f6b475c7787fb79a2f0
AlamAbdillah <115331322+AlamAbdillah@users.noreply.github.com>
2022-12-01 09:09:01 +0700

    Update update.php

--- update.php
+++ update.php
@@ -18,7 +18,7 @@ $update_total = $row['total'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kedai Cesa</title>
+    <title>My Toko</title>
     <link rel="stylesheet" type="text/css" href="css/update.css">
 </head>
 <body>
